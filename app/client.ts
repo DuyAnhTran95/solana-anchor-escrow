d758a7ee2f19b627611f596b5b7c7c9ad221f73b
Duy Anh <duyanh.tran8995@gmail.com>
2022-02-10 18:11:47 +0700

    first commit

---
+++ app/client.ts
@@ -0,0 +1,112 @@
+import * as anchor from "@project-serum/anchor";
+import { PublicKey, SOLANA_SCHEMA, SystemProgram } from "@solana/web3.js";
+import * as fs from "fs";
+import { Token, TOKEN_PROGRAM_ID, AccountLayout } from "@solana/spl-token";
+require("dotenv").config();
+
+const idl = JSON.parse(
+  fs.readFileSync("./target/idl/anchor_test.json", "utf-8")
+);
+
+const secretKey = Uint8Array.from(
+  JSON.parse(fs.readFileSync(process.env.PAYER_PK, "utf-8"))
+);
+
+const programId = new anchor.web3.PublicKey(idl.metadata?.address);
+
+const escrowAcc = anchor.web3.Keypair.generate();
+const initializer = anchor.web3.Keypair.generate();
+const mintAuthority = anchor.web3.Keypair.generate();
+const vaultAcc = new anchor.web3.Keypair();
+const payer = anchor.web3.Keypair.fromSecretKey(Uint8Array.from(secretKey));
+
+let initializerTokenAccA: PublicKey;
+let initializerTokenAccB: PublicKey;
+let mintA: Token, mintB: Token;
+
+const provider = anchor.Provider.local();
+
+anchor.setProvider(provider);
+
+const escrowProgram = new anchor.Program(idl, programId);
+
+async function setup() {
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(initializer.publicKey, 10000000000),
+    "processed"
+  );
+
+  mintA = await Token.createMint(
+    provider.connection,
+    payer,
+    mintAuthority.publicKey,
+    mintAuthority.publicKey,
+    6,
+    TOKEN_PROGRAM_ID
+  );
+
+  mintB = await Token.createMint(
+    provider.connection,
+    payer,
+    mintAuthority.publicKey,
+    mintAuthority.publicKey,
+    6,
+    TOKEN_PROGRAM_ID
+  );
+
+  initializerTokenAccA = await mintA.createAccount(initializer.publicKey);
+  initializerTokenAccB = await mintB.createAccount(initializer.publicKey);
+
+  await mintA.mintTo(
+    initializerTokenAccA,
+    mintAuthority.publicKey,
+    [mintAuthority],
+    1000
+  );
+}
+
+async function main() {
+  await setup();
+
+  await escrowProgram.rpc.initEscrow(new anchor.BN(10), {
+    accounts: {
+      user: initializer.publicKey,
+      tokenDepositAcc: initializerTokenAccA,
+      tokenRxAcc: initializerTokenAccB,
+      escrowInfo: escrowAcc.publicKey,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      vaultAcc: vaultAcc.publicKey,
+    },
+    instructions: [
+      SystemProgram.createAccount({
+        programId: TOKEN_PROGRAM_ID,
+        space: AccountLayout.span,
+        lamports: await provider.connection.getMinimumBalanceForRentExemption(
+          AccountLayout.span
+        ),
+        fromPubkey: initializer.publicKey,
+        newAccountPubkey: vaultAcc.publicKey,
+      }),
+      Token.createInitAccountInstruction(
+        TOKEN_PROGRAM_ID,
+        mintA.publicKey,
+        vaultAcc.publicKey,
+        initializer.publicKey
+      ),
+      // await escrowProgram.account.escrow.createInstruction(escrowAcc),
+    ],
+    signers: [escrowAcc, initializer, vaultAcc],
+  });
+
+  let _escrowAcc = await escrowProgram.account.escrow.fetch(
+    escrowAcc.publicKey
+  );
+
+  console.log(_escrowAcc);
+}
+
+main().then((rs) => {
+  console.log("done");
+});
---
+++ programs/anchor-test/src/lib.rs
@@ -0,0 +1,80 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, SetAuthority, TokenAccount};
+use spl_token::instruction::AuthorityType;
+
+declare_id!("HKu3iNjUuQbimNN4W6qZahvXn4rchaESwcPvsV1sdQsW");
+
+#[program]
+pub mod anchor_test {
+    use super::*;
+
+    const ESCROW_PDA_SEED: &[u8] = b"escrow";
+
+    pub fn init_escrow(ctx: Context<InitializeEscrow>, expected_amount: u64) -> ProgramResult {
+        let escrow_info = &mut ctx.accounts.escrow_info;
+        escrow_info.is_initialized = true;
+        escrow_info.initializer = *ctx.accounts.user.key;
+        escrow_info.initializer_rx_acc =
+            *ctx.accounts.token_rx_acc.to_account_info().key;
+        escrow_info.escrow_token_acc = *ctx.accounts.token_program.key;
+        escrow_info.expected_amount = expected_amount;
+
+        let (vault_pda, _vault_bump) =
+            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+
+        let set_auth_cpi = SetAuthority {
+            current_authority: ctx.accounts.user.to_account_info().clone(),
+            account_or_mint: ctx.accounts.vault_acc.to_account_info().clone(),
+        };
+        let auth_cpi_ctx = CpiContext::new(ctx.accounts.token_program.clone(), set_auth_cpi);
+        token::set_authority(auth_cpi_ctx, AuthorityType::AccountOwner, Some(vault_pda))?;
+
+        Ok(())
+    }
+
+    pub fn exchange(_ctx: Context<Exchange>) -> ProgramResult {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeEscrow<'info> {
+    #[account(init, payer = user, space = 8 + 8 + 32 * 3  + 8)]
+    pub escrow_info: Account<'info, Escrow>,
+    #[account(mut @ EscrowError::InvalidAccount)]
+    pub user: Signer<'info>,
+    #[account(mut @ EscrowError::InvalidAccount)]
+    pub token_deposit_acc: Account<'info, TokenAccount>,
+    pub token_rx_acc: Account<'info, TokenAccount>,
+    pub token_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    #[account(mut @ EscrowError::InvalidAccount)]
+    pub vault_acc: Account<'info, TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct Exchange<'info> {
+    // #[account(mut, has_one = authority)]
+    // pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct Escrow {
+    pub is_initialized: bool,
+    pub initializer: Pubkey,
+    pub escrow_token_acc: Pubkey,
+    pub initializer_rx_acc: Pubkey,
+    pub expected_amount: u64,
+}
+
+#[error]
+pub enum EscrowError {
+    #[msg("Invalid account")]
+    InvalidAccount,
+    #[msg("Invalid account 2")]
+    InvalidAccount2,
+    #[msg("Invalid account 3")]
+    InvalidAccount3,
+}
